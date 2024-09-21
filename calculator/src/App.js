748c8c62d3b322ad4b9c207952037fdde6a85d89
samettopcu88 <samet.topcu.88@icloud.com>
2024-09-21 18:47:41 +0300

    güncelleme shape circle

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -12,15 +12,55 @@ function App() {
 const Calculator = () => {
   return(
     <div className="calculator">
+
       <div className="answer-section">
-        <h1>72</h1>
+        <h1 className="answer">72</h1>
       </div>
+
       <div className="button-section">
 
+        <div className="button-row"> 
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+        </div>
+        <div className="button-row"> 
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+        </div>
+        <div className="button-row"> 
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+        </div>
+        <div className="button-row"> 
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+        </div>
+        <div className="button-row"> 
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+          <CalculatorButton shape="circle" />
+        </div>
       </div>
+
     </div>
   )
 
+}
+
+const CalculatorButton = ({color, shape}) => {
+  return(
+    <div className={"calculator-button ${color] ${shape} "}>
+
+    </div>
+  )
 }
 
 export default App;
